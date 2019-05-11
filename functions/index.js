60e75fca0645475141a7d3b1a678e6c19352e565
Kazuhiro Seo <kazuhiro1982@gmail.com>
2019-05-12 01:44:32 +0900

    DB操作サンプル作成

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,31 @@
 const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+admin.initializeApp(functions.config().firebase);
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 exports.putStatus = functions.https.onRequest((request, response) => {
- response.send("puts status");
+  if (request.method !== 'PUT') {
+    response.status(405).send('Method Not Allowed');
+    return;
+  }
+  let cinemaId = request.body.cinemaId;
+  let status = request.body.status;
+  admin.database().ref("/cinema/" + cinemaId).set({
+    status: status
+  });
+  response.send("OK");
+});
+
+exports.summary = functions.https.onRequest((request, response) => {
+  let cinemaId = request.query.cinemaId;
+  admin.database().ref("/vote/" + cinemaId).once('value')
+  .then(result => {
+    response.send(result);
+  })
+  .catch(error => {
+    response.status(404).send({ message: 'Not Found' })
+  });
 });
 
 exports.helloWorld = functions.https.onRequest((request, response) => {
